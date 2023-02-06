d9d0b9f453178ae3711525a3ff39fe87969e0138
aishna16 <aishna394@gmail.com>
2023-02-05 22:56:02 -0330

    aishna

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-print ("aishna16")
-=======
-Change made by girish
->>>>>>> e1e6083fb76c76f74f6938da77fdc4e332336b54
+#Change made by girish
+#>>>>>>> e1e6083fb76c76f74f6938da77fdc4e332336b54
+#yes
