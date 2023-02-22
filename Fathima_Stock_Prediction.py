8a7e2ad88d349acbf8d03435bb39bed684f1e2aa
Fathima <iamfathima293@gmail.com>
2023-02-22 01:12:23 -0330

    Removed unnecessary lines

--- Fathima_Stock_Prediction.py
+++ Fathima_Stock_Prediction.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-data = pd.read_csv("https://raw.githubusercontent.com/GirishVerm/Stock_Prediction_Model/main/AAPL%20(1).csv",on_bad_lines='skip')
+data = pd.read_csv("https://raw.githubusercontent.com/GirishVerm/Stock_Prediction_Model/main/AAPL%20(1).csv")
 
 print(data)
 
