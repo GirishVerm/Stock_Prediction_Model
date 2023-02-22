996b9fc5840a242ffa27aeeb02bc07884b191a5f
GirishVerm <verma.girish2002@gmail.com>
2023-02-22 01:04:29 -0330

    girish commit

--- Girish_Stock_Prediction.py
+++ Girish_Stock_Prediction.py
@@ -1,12 +1,19 @@
 # -- This is Girish file
-# -- Predicting open stock prices
+# -- Predicting Low stock prices
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
 data = pd.read_csv("https://raw.githubusercontent.com/GirishVerm/Stock_Prediction_Model/main/AAPL%20(1).csv", on_bad_lines='skip')
-print(data)
+
+
+LowPricesList = []
+for row in data['Low']:
+    LowPricesList.append(row)
+
+print(LowPricesList)
+
 
 
 
@@ -14,6 +21,6 @@ yarray = np.array([0,1,6,4,7,2,7,5,4])
 
 plt.plot(yarray, marker = 'o')
 
-plt.show()
+#plt.show()
 
 
