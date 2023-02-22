e205c565d1ebc78312a0a928270e4ce58a8542b0
Fathima <iamfathima293@gmail.com>
2023-02-22 01:05:34 -0330

    Made column of Open

--- Fathima_Stock_Prediction.py
+++ Fathima_Stock_Prediction.py
@@ -4,9 +4,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-data = pd.read_csv("https://github.com/GirishVerm/Stock_Prediction_Model/blob/main/AAPL%20(1).csv",on_bad_lines='skip')
+data = pd.read_csv("https://raw.githubusercontent.com/GirishVerm/Stock_Prediction_Model/main/AAPL%20(1).csv",on_bad_lines='skip')
 
 print(data)
+
+openList=data["Open"].values.tolist()
+
+print(openList)
 
 yarray = np.array([0,1,6,4,7,2,7,5,4])
 
