461e846dd7fb7ea6235e59f2505c6dd25d4f4f02
GirishVerm <verma.girish2002@gmail.com>
2023-02-22 00:05:50 -0330

    girish commit

--- Girish_Stock_Prediction.py
+++ Girish_Stock_Prediction.py
@@ -1,4 +1,13 @@
 # -- This is Girish file
 # -- Predicting open stock prices
 
-# --  Change 1
+import matplotlib.pyplot as plt
+import numpy as np
+
+yarray = np.array([0,1,6,4,7,2,7,5,4])
+
+plt.plot(yarray, marker = 'o')
+
+plt.show()
+
+
