cf0807e763110b5ed074ee85011763b495403ddd
Fathima <iamfathima293@gmail.com>
2023-02-22 00:32:40 -0330

    reading csv file

--- Fathima_Stock_Prediction.py
+++ Fathima_Stock_Prediction.py
@@ -1,2 +1,13 @@
-import matplotlib
-print(matplotlib.__version__)
+#*
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+data = pd.read_csv("https://github.com/GirishVerm/Stock_Prediction_Model/blob/main/AAPL%20(1).csv",on_bad_lines='skip')
+
+yarray = np.array([0,1,6,4,7,2,7,5,4])
+
+plt.plot(yarray, marker = 'o')
+
+plt.show()
