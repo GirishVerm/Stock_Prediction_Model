b27cebba63dd50b4c0df579f30b3f3424d464577
aishna16 <aishna394@gmail.com>
2023-02-22 01:13:09 -0330

    data as a list

--- Aishna_Stock_Prediction.py
+++ Aishna_Stock_Prediction.py
@@ -1,11 +1,11 @@
 #aishnabest
-#✰✰
+#✰✰✰
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-data = pd.read_csv("https://raw.githubusercontent.com/GirishVerm/Stock_Prediction_Model/main/AAPL%20(1).csv", header=None)
+data = pd.read_csv("https://raw.githubusercontent.com/GirishVerm/Stock_Prediction_Model/main/AAPL%20(1).csv")
 
 yarray = np.array([0,1,6,4,7,2,7,5,4])
 
@@ -14,3 +14,10 @@ plt.plot(yarray, marker = 'o')
 #plt.show()
 
 print(data)
+
+highPriceList = []
+
+for row in data["High"]:
+    highPriceList.append(row)
+
+print (highPriceList)
